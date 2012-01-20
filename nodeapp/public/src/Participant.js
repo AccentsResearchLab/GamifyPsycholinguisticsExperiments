8a4826f515647ced4611dc86bbc48ca0e6ea62cc
cesine <cesine@host>
2012-01-19 20:14:44 -0500

    adding callbacks

--- nodeapp/public/src/Participant.js
+++ nodeapp/public/src/Participant.js
@@ -1,4 +1,56 @@
-surveyClick = function(event){
+var Participant = function(){
+	/*
+	 * Variables gathered from browser on participant creation, used as indirect
+	 * measures of the participant's native language(s) and other factors that need
+	 * to be controlled for.
+	 */
+	this.systemLocale = "";
+	this.userAgentString = "";
+	this.timezone = "";
+	this.ipaddress = "";
+	this.startTime = "";
+	this.exitTime = "";
+	this.sessionId = "";
+	/*
+	 * Variables gathered during the exit survey if the participant wants to 
+	 * participate.
+	 */
+	this.surveyResults = [];
+	/*
+	 * Variables used if the participant wants to register for extra credit.
+	 */
+	this.email = "";
+};
+
+Participant.prototype.create = function(callback){
+	debug("Inserting new participant.");
+	if(typeof callback === 'function'){
+		callback();
+	}
+};
+Participant.prototype.read = function(callback){
+	debug("Reading participant.");
+	if(typeof callback === 'function'){
+		callback();
+	}
+};
+Participant.prototype.update = function(newInfo, callback){
+	debug("Updating participant.");
+	if(typeof callback === 'function'){
+		callback();
+	}
+};
+Participant.prototype.del = function(callback){
+	debug("Deleting participant");
+	if(typeof callback === 'function'){
+		callback();
+	}
+};
+
+surveyClick = function(event, callback){
 	debug("User clicked a survey button: "+event.target.value);
 	debug("The category was: "+event.target.parentNode.id);
+	if(typeof callback === 'function'){
+		callback();
+	}
 };
--- nodeapp/public/src/Stimuli.js
+++ nodeapp/public/src/Stimuli.js
@@ -1,10 +1,22 @@
-adjustGain = function(event) {
+adjustGain = function(event, callback) {
 	debug("User adjusted the gain to: "+event.target.value);
+	if(typeof callback === 'function'){
+		callback();
+	}
 };
-playAudio = function(event){
+playAudio = function(event, onAudioStarted, onAudioFinished){
 	debug("User clicked play.");
+	if(typeof onAudioStarted === 'function'){
+		onAudioStarted();
+	}
+	if(typeof onAudioFinished === 'function'){
+		onAudioFinished();
+	}
 };
 
-playSoundCheckAudio = function(event){
+playSoundCheckAudio = function(event, callback){
 	debug("User clicked play sound check.");
+	if(typeof callback === 'function'){
+		callback();
+	}
 };
