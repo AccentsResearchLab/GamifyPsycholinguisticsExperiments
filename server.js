5ce8d57c26c520990486f37e21c358280569aa20
cesine <cesine@host>
2012-01-20 14:34:09 -0500

    hacking to get the routing to run for now...

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/',function(req,res){ 
+ 	res.redirect('/view/soundcheck.html');
 });
+app.post('/view/main.html',function(req,res){
+  res.redirect('/view/main.html');
+});
+app.post('/view/survey.html',function(req,res){
+  res.redirect('/view/survey.html');
+});
+
+
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
