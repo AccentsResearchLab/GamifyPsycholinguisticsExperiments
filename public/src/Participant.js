1549086908cf4c000649c1e628a845623901884f
cesine <cesine@host>
2012-01-20 19:24:51 -0500

    added backbone to particiant but since there is no maniulation of the model, no view of the model except as a row in a spreadsheet backbone seems rather unnecesary at this point. closes #5

--- public/src/Participant.js
+++ public/src/Participant.js
@@ -1,51 +1,36 @@
-var Participant = function(){
+var Participant = Backbone.Model.extend({
+	defaults: {
 	/*
 	 * Variables gathered from browser on participant creation, used as indirect
 	 * measures of the participant's native language(s) and other factors that need
 	 * to be controlled for.
 	 */
-	this.systemLocale = "";
-	this.userAgentString = "";
-	this.timezone = "";
-	this.ipaddress = "";
-	this.startTime = "";
-	this.exitTime = "";
-	this.sessionId = "";
-	/*
-	 * Variables gathered during the exit survey if the participant wants to 
-	 * participate.
-	 */
-	this.surveyResults = [];
-	/*
-	 * Variables used if the participant wants to register for extra credit.
-	 */
-	this.email = "";
-};
+		systemLocale : '',
+		browser : '',
+		timezone : '',
+		ipaddress : '',
+		startTime : '',
+		exitTime : '',
+		sessionId : '',
+		surveyResults : [],
+		votes : []
+	},
+	initialize: function(){
+		debug("Welcome new participant.");
+		this.bind("change:votes", function(){
+			debug(this.attributes);
+		})
+	},
+	validate: function( attributes ){
+		debug("validating");
+	},
+	vote: function( value ){
+		var votes_array = this.get("votes");
+		votes_array.push( value );
+		this.set({ votes: votes_array });
+	}
+});
 
-Participant.prototype.create = function(callback){
-	debug("Inserting new participant.");
-	if(typeof callback === 'function'){
-		callback();
-	}
-};
-Participant.prototype.read = function(callback){
-	debug("Reading participant.");
-	if(typeof callback === 'function'){
-		callback();
-	}
-};
-Participant.prototype.update = function(newInfo, callback){
-	debug("Updating participant.");
-	if(typeof callback === 'function'){
-		callback();
-	}
-};
-Participant.prototype.del = function(callback){
-	debug("Deleting participant");
-	if(typeof callback === 'function'){
-		callback();
-	}
-};
 
 surveyClick = function(event, callback){
 	debug("User clicked a survey button: "+event.target.value);
