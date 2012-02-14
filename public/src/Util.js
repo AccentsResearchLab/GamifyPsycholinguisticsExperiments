58d54ca5ec822af7718999e60632a201213f24a8
cesine <cesine@host>
2012-02-13 20:36:03 -0500

    add the three language game buttons for #13 using localstorage to keep track of the game between pages

--- public/src/Util.js
+++ public/src/Util.js
@@ -1,3 +1,28 @@
+
+/*
+ * Adding and removing class on DOM elements in a safe manner
+ * incase there are mulitiple classes
+ */
+function hasClass(ele,cls) {
+  return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+function addClass(ele,cls) {
+  if (!this.hasClass(ele,cls)) ele.className += " "+cls;
+}
+function removeClass(ele,cls) {
+  if (hasClass(ele,cls)) {
+      var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)','g');
+      ele.className=ele.className.replace(reg,' ');
+  }
+}
+
 debug = function(message){
 	console.log(message);
 };
+
+setGameLanguage = function(event){
+	localStorage.setItem("gameLanguage",event.target.value);
+	removeClass(document.getElementById("soundcheckbuttons"), "hidden");
+	addClass(document.getElementById("choosegamebuttons"), "hidden");
+	
+}
