95744200b26abcccb55f8ad4b5cdd410986a6555
cesine <cesine@host>
2012-02-13 20:54:28 -0500

    added play sound check/mission audio once teh user clicks on their langauge choice. for #15

--- public/src/Util.js
+++ public/src/Util.js
@@ -25,5 +25,18 @@ setGameLanguage = function(event){
 	localStorage.setItem("gameLanguage",event.target.value);
 	removeClass(document.getElementById("soundcheckbuttons"), "hidden");
 	addClass(document.getElementById("choosegamebuttons"), "hidden");
+	playGameMission(event.target.value);
 	
+}
+playGameMission = function(lang){
+	debug("Playing mission for "+lang);
+	el = $("<audio></audio>");
+	if(lang == "Russian"){
+		el.attr("src", "./../audio_stimuli/SbeatD.wav");//TODO put mission audio here
+	}else if(lang=="Sussex"){
+		el.attr("src", "./../audio_stimuli/SbeatD.wav");//TODO put mission audio here
+	}else if(lang=="South African"){
+		el.attr("src", "./../audio_stimuli/SbeatD.wav");//TODO put mission audio here
+	}
+	el.get(0).play();
 }
