4b4b54c9183f0b9b101b1022d1fdd8af4f80fb54
cesine <cesine@yahoo.com>
2012-03-14 22:24:46 -0400

    making the meter larger

--- public/src/AccentMeter.js
+++ public/src/AccentMeter.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("accent_meter");
 var ctx = canvas.getContext("2d");
-canvas.width = 180;
-canvas.height = 450;
+canvas.width = 160;
+canvas.height = 500;
 var img = new Image();
     img.onload = function(){
       ctx.drawImage(img,0,0,canvas.width,canvas.height);
