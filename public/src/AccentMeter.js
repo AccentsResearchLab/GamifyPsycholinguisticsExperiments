462b11856afc83d139041a172f791f7b7dc08c5a
cesine <cesine@yahoo.com>
2012-03-14 22:20:45 -0400

    making the spy meter larger, and adding some more finnesse to the slider with hot and cold icons

--- public/src/AccentMeter.js
+++ public/src/AccentMeter.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById("accent_meter");
 var ctx = canvas.getContext("2d");
-canvas.width = 105;
-canvas.height = 330;
+canvas.width = 180;
+canvas.height = 450;
 var img = new Image();
     img.onload = function(){
-      ctx.drawImage(img,0,0);
+      ctx.drawImage(img,0,0,canvas.width,canvas.height);
       /*TODO draw the scale */
       ;
     };
