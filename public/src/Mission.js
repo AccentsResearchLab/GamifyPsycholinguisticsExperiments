80123654958bc4b37db00d4cc073778ba8563356
cesine <cesine@yahoo.com>
2012-04-07 13:49:22 -0400

    changed the way the missions were playing, added play and pause controls and saved whether the user listend to the audio or not

--- public/src/Mission.js
+++ public/src/Mission.js
@@ -11,14 +11,35 @@ setGameLanguage = function(gameindex){
 	//playGameMission(currentGame);
 	localStorage.setItem( "games", JSON.stringify(games) );
 	document.getElementById("mission_text").innerHTML= games[currentGame].description;
+	el = document.getElementById("mission_audio");
+	el.setAttribute("src", games[currentGame].missionAudio);
 
-	
 }
-playGameMission = function(){
+playGameMission = function(event){
+	if(event.target.value =="Pause"){
+		event.target.value = "Play Sound Check";
+		el = document.getElementById("mission_audio");
+		el.pause();
+		var visitor = localStorage.getItem("visitor");
+		if(visitor){
+			visitor = JSON.parse(visitor);
+			visitor["userlistenedtomission"] = visitor["userlistenedtomission"] +",Pause:"+ Date.now();
+			localStorage.setItem("visitor",JSON.stringify(visitor));
+		}
+		return;
+	}
 	debug("Playing mission for "+games[currentGame].language);
-	el = $("<audio></audio>");
-	el.attr("src", games[currentGame].missionAudio);//TODO put mission audio here
-	el.get(0).play();
+	el = document.getElementById("mission_audio");
+	el.play();
+	event.target.value="Pause";
+
+	var visitor = localStorage.getItem("visitor");
+	if(visitor){
+		visitor = JSON.parse(visitor);
+		visitor["soundCheckAudioDuration"] = Math.round(el.duration*1000);
+		visitor["userlistenedtomission"] =  visitor["userlistenedtomission"] +",Play:"+ Date.now();
+		localStorage.setItem("visitor",JSON.stringify(visitor));
+	}
 }
 
 var tryToGuessDialect = function(){
@@ -39,6 +60,7 @@ var tryToGuessDialect = function(){
 	if (visitor.navigatorlanguage.toLowerCase().indexOf("ru") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("ru") == 0 || visitor.countrycode.indexOf("RU") > -1 || visitor.countrycode.indexOf("BY") > -1 || visitor.countrycode.indexOf("UA") > -1 ){
 		sus_button = false;
 		southaf_button = false;
+		setGameLanguage(0);
 	}else 
 	/*
 	If the visitor has south african english set, or they are in South Africa, send them to the South African game
@@ -46,6 +68,7 @@ var tryToGuessDialect = function(){
 	if(visitor.navigatorlanguage.toLowerCase().indexOf("en-za") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("en-za") == 0 || visitor.countrycode.indexOf("ZA") > -1 ){
 		rus_button  = false;
 		sus_button = false;
+		setGameLanguage(2);
 	}else
 	/*
 	If the visitor has british english set, or they are the UK, send them to the Sussex game
@@ -53,6 +76,7 @@ var tryToGuessDialect = function(){
 	if(visitor.navigatorlanguage.toLowerCase().indexOf("en-gb") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("en-gb") == 0 || visitor.countrycode.indexOf("GB") > -1 ){
 		rus_button  = false;
 		southaf_button = false;
+		setGameLanguage(1);
 	}
 	var r = document.getElementById("rus_button");
 	var s = document.getElementById("sus_button");
@@ -60,6 +84,7 @@ var tryToGuessDialect = function(){
 	if(r && !rus_button){
 		r.disabled=true;
 		r.setAttribute("class","button");
+
 	}
 	if(s && !sus_button){
 		s.disabled=true;
