534b69c91a3bb9f52f9d93b32d664b2c44cfefb7
cesine <cesine@yahoo.com>
2012-04-07 13:00:23 -0400

    disabling buttons depending on where the user is or their language. this is incase we want them to see the possible other dialects. in the next commit i will set the language automatically if the browser is confident of the participant's dialect fixes #24

--- public/src/Mission.js
+++ public/src/Mission.js
@@ -1,7 +1,7 @@
 loadData("./../json/game_description.json", null);
 
-setGameLanguage = function(event){
-	window.currentGame = event.target.value;
+setGameLanguage = function(gameindex){
+	window.currentGame = gameindex;
 
 	var language = games[currentGame].language;
 	debug("Setting language to "+language);
@@ -20,3 +20,56 @@ playGameMission = function(){
 	el.attr("src", games[currentGame].missionAudio);//TODO put mission audio here
 	el.get(0).play();
 }
+
+var tryToGuessDialect = function(){
+	var visitor = localStorage.getItem("visitor");
+	if(!visitor){
+		return;
+	}
+	var rus_button = true;
+	var sus_button = true;
+	var southaf_button = true;
+
+	visitor = JSON.parse(visitor);
+	/*
+	If visitor is currently in Russia, Belarus or Ukraine send them to the Russian game, 
+	or their browser is in russian, 
+	or if their acceptlanguage starts with russian.
+	*/
+	if (visitor.navigatorlanguage.toLowerCase().indexOf("ru") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("ru") == 0 || visitor.countrycode.indexOf("RU") > -1 || visitor.countrycode.indexOf("BY") > -1 || visitor.countrycode.indexOf("UA") > -1 ){
+		sus_button = false;
+		southaf_button = false;
+	}else 
+	/*
+	If the visitor has south african english set, or they are in South Africa, send them to the South African game
+	*/
+	if(visitor.navigatorlanguage.toLowerCase().indexOf("en-za") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("en-za") == 0 || visitor.countrycode.indexOf("ZA") > -1 ){
+		rus_button  = false;
+		sus_button = false;
+	}else
+	/*
+	If the visitor has british english set, or they are the UK, send them to the Sussex game
+	*/
+	if(visitor.navigatorlanguage.toLowerCase().indexOf("en-gb") > -1 || visitor.httpheaderslanguage.toLowerCase().indexOf("en-gb") == 0 || visitor.countrycode.indexOf("GB") > -1 ){
+		rus_button  = false;
+		southaf_button = false;
+	}
+	var r = document.getElementById("rus_button");
+	var s = document.getElementById("sus_button");
+	var a = document.getElementById("southaf_button");
+	if(r && !rus_button){
+		r.disabled=true;
+		r.setAttribute("class","button");
+	}
+	if(s && !sus_button){
+		s.disabled=true;
+		s.setAttribute("class","button");
+	}
+	if(a && !southaf_button){
+		a.disabled=true;
+		a.setAttribute("class","button");
+	}
+
+
+}
+window.setTimeout("tryToGuessDialect();", 2000);
