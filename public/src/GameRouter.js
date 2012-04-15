8961d06691ad0ff15f4ac5de111670140b51ef36
cesine <cesine@yahoo.com>
2012-04-14 22:56:03 -0400

    making the layout better

--- public/src/GameRouter.js
+++ public/src/GameRouter.js
@@ -2,7 +2,6 @@
 var GameRouter = Backbone.Router.extend({
 	initialize: function(){
 		
-		window.games = JSON.parse(localStorage.getItem("games"));
 		this.participant = new ParticipantModel();
 		this.scores = [];
 		this.scores[0] = [
@@ -18,11 +17,6 @@ var GameRouter = Backbone.Router.extend({
 		// alert("Game language is not set, please click on a language.");
 		// window.location="soundcheck.html";
 		}
-		document.getElementById("spytype").innerHTML=window.games[this.activeGame].adjective +" ";
-		document.getElementById("spytype2").innerHTML=window.games[this.activeGame].language +" ";
-		document.getElementById("spytype3").innerHTML=window.games[this.activeGame].language +" ";
-		
-		debug("Game language is set to "+window.games[this.activeGame].language);
 		this.stimuliJson = window.games[this.activeGame].stimuliJson;
 		window.game = this;
 		loadSamples( this.stimuliJson, startGame() );
@@ -38,6 +32,12 @@ var GameRouter = Backbone.Router.extend({
 
 var startGame = function(){
 	debug("Starting game.");
+	document.getElementById("spytype").innerHTML=window.games[window.game.activeGame].adjective +" ";
+	document.getElementById("spytype2").innerHTML=window.games[window.game.activeGame].language +" ";
+	document.getElementById("spytype3").innerHTML=window.games[window.game.activeGame].language +" ";
+	
+	debug("Game language is set to "+window.games[window.game.activeGame].language);
+		
 	draw_counter(window.game.countAudioInSet);
 	var visitor = localStorage.getItem("visitor");
 	if(visitor){
