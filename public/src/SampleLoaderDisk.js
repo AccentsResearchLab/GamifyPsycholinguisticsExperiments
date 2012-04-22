00bc6b2d16c4cb89b84897b685d261fff000537c
cesine <cesine@yahoo.com>
2012-04-22 14:55:47 -0400

    debugging

--- public/src/SampleLoaderDisk.js
+++ public/src/SampleLoaderDisk.js
@@ -1,12 +1,13 @@
 // This is a simple "sample loader" which will load the stimuli
 // description from a json file on the disk, or from a server
 loadStimuliList = function(filename, callback) {
+  debug("Loading stimuli");
   var logicURL = localStorage.getItem("logicUrl");
   if(logicURL.length > 0){
     console.log(logicURL+"/stimuliOrder/"+filename.replace("./../json/audio_stimuli_","").replace(".json",""));
     $.getJSON(logicURL+"/stimuliOrder/"+filename.replace("./../json/audio_stimuli_","").replace(".json",""), function(data) {
       window.game.stimuli = data;
-      debug(data.length);
+      
       //TODO if remote data has a problem, load the local one.
       if(typeof callback === 'function'){
         callback();
@@ -26,6 +27,7 @@ loadData = function(filename, callback){
 	debug("will read in data");
   $.getJSON(filename, function(data) {
   	window.games = data;
+    debug(JSON.stringify(window.games));
     if(typeof callback === 'function'){
       callback();
     }
