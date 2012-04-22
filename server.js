d171b558d34f62a9fac99464b7913988b52049e9
cesine <cesine@yahoo.com>
2012-04-21 23:02:30 -0400

    added an api to retrieve the stimuli order from the server, so we can later add more complicated logic to return stimuli that need more participants

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
-
+var fs = require('fs');
+var russianJSON = JSON.parse(fs.readFileSync('./public/json/audio_stimuli_russian.json'));
+var sussexJSON = JSON.parse(fs.readFileSync('./public/json/audio_stimuli_sussex.json'));
+var southAfricanJSON = JSON.parse(fs.readFileSync('./public/json/audio_stimuli_southafrican.json'));
+var _ = require('underscore');
 var app = express.createServer(express.logger());
 
 app.get('/',function(req,res){ 
  	res.redirect('/view/index.html');
+});
+app.get('/stimuliOrder/russian',function(req,res){ 
+	res.writeHead(200, {'content-type': 'application/json'});
+    res.write(JSON.stringify(_.shuffle(russianJSON)));
+    res.end();
+});
+app.get('/stimuliOrder/sussex',function(req,res){ 
+	res.writeHead(200, {'content-type': 'application/json'});
+    res.write(JSON.stringify(_.shuffle(sussexJSON)));
+    res.end();
+});
+app.get('/stimuliOrder/southafrican',function(req,res){ 
+	res.writeHead(200, {'content-type': 'application/json'});
+    res.write(JSON.stringify(_.shuffle(southAfricanJSON)));
+    res.end();
 });
 app.post('/view/main.html',function(req,res){
   res.redirect('/view/main.html');
@@ -19,3 +38,4 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
